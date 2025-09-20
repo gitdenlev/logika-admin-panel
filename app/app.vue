61a9bf04edfb6d62c92168882e819d968e5fde6a
gitdenlev <levchenkoden02@gmail.com>
2025-09-20 15:57:04 +0300

    add config google search console

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
 // Import the Toaster component and Vue Sonner styles
-import { Toaster } from '@/components/ui/sonner';
-import 'vue-sonner/style.css';
+import { Toaster } from "@/components/ui/sonner";
+import "vue-sonner/style.css";
+useHead({
+  meta: [
+    {
+      name: "google-site-verification",
+      content: "2up4kJbnI0aA7lxr_nO8-YZuYZ1846nb_WhHUNJtEXs",
+    },
+  ],
+});
 </script>
 
 <template>
   <div>
     <!-- Render full page content using NuxtPage component -->
     <NuxtPage />
-    
+
     <!-- Display toaster component with rich colors and expandable options -->
     <Toaster richColors :expand="true" />
   </div>
